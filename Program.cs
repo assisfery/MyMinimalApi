df00bea8e4e3fa81c6835ddf5d76b9595c208c02
Assis Ferreira <ferreira.assisruny@gmail.com>
2024-01-14 11:28:34 +0000

    feat: list and todo endpoint

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/todo", async (TodoDb db) => {
+    return await db.Todos.ToListAsync();
+});
+
+app.MapPost("/todo", async (Todo todo, TodoDb db) => {
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+    return Results.Created("todo/" + todo.Id, todo);
+});
+
 
 app.Run();
